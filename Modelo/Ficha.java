c8b800273491e2c08d03ba397e8576c23806339a
mario mendoza <javi_mendoza8@hotmail.com>
2014-09-11 10:03:18 +0100

    organizando

---
+++ Modelo/Ficha.java
@@ -0,0 +1,16 @@
+package Modelo;
+
+public class Ficha
+{
+    private char ficha;
+    /*Constructor de la clase Ficha*/
+    public Ficha(char fic)
+    {
+        ficha=fic;
+    }
+    /*Metodo que retorna el tipo de ficha*/
+	public char getFicha()
+	{
+	    return ficha;
+	}
+}
