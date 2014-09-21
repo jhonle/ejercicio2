4f6c9ad9f79a21d276f23bf309e8cae39b216119
mario mendoza <javi_mendoza8@hotmail.com>
2014-09-21 11:31:58 +0100

    creando carpeta Pruebas

---
+++ Pruebas/Main.java
@@ -0,0 +1,21 @@
+package Pruebas;
+
+import Controlador.Controlador;
+import Modelo.*;
+import Vista.VentanaDeIngreso;
+import Vista.VentanaDeJuego;
+
+public class Main 
+{		        
+	/**
+	 * Metodo main inicio de Juego
+     * 
+     * */	    
+	public static void main(String[] args) 	    
+	{	    	
+		new Controlador();	        	    
+	}	
+}
+
+
+
